94eadca088d6959cc0cbbb5d85c8361b8207f456
Sam Tolkin <sbtolkin4@gmail.com>
2016-01-09 16:03:08 -0500

    Got points and runes showing
    Need to work on getting the cards to look good when they appear.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,53 @@
 (function() {
 	var app = angular.module('gameView', []);
+	var tempGame = function () { //Used to fully populate a game for front-end updates
+		this.id = null;
+		this.name = '';
+		this.turn = null;
+		this.players = [];
+		this.deck = [];
+		this.scrap = [];
+		this.topCard = {};
+		this.secondCard = {};
+		this.scrapTop = {};
+		this.firstEffect = {};
+		this.twos = [];
+		this.winner = null;
+		this.log = [];
+	};
+
+	var tempPlayer = function () {
+		this.id = null;
+		this.socketId = '';
+		this.pNum = null;
+		this.currentGame = null;
+		this.points = [];
+		this.runes = [];
+		this.hand = [];
+	};
+
+	var Card = function (suit, rank) {
+		this.suit = suit;
+		this.rank = rank;
+		this.image = '../images/bicycleBackBlue.jpg';
+		this.attachments = [];
+	}
+
+	var newGame = new tempGame();
+	var you = new tempPlayer();
+	var opponent = new tempPlayer();
+	var tempCard = new Card(3, 1);
+	var tempJackCard = new Card(0, 1);
+	tempJackCard.attachments = [tempCard];
+	you.hand = [tempCard, tempCard, tempCard];
+	opponent.hand = [tempCard, tempCard, tempCard];
+	you.points = [tempCard, tempJackCard];
+	opponent.points = [tempJackCard];
+	newGame.players = [you, opponent];
 
 	app.controller('gameViewController', function($scope, $rootScope) {
-		this.foo = "foo";
-		this.boo = "boo";
+		this.game = newGame;
+		this.pNum = 0;
+
 	});
 })();
